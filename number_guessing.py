ffca831f85cc9c60e7ee38e59bbc2dc0ce50868b
KluJa <ynhojk@gmail.com>
2018-05-22 14:25:39 +0200

    Added methods to number_guessing

--- number_guessing.py
+++ number_guessing.py
@@ -1,16 +1,22 @@
 import random
 
-magic_numbers = random.randint(0,9)
-chances = 3
-
-print("{}".format(magic_numbers))
-
-for attempt in range(chances):
-    print("Attempt number: {}".format(attempt+1))
+def ask_user_and_check_reply():
     user_number = int(input("Enter a number between 0 and 9: "))
     if user_number ==  magic_numbers:
-        print("You got the number right!")
+        return ("You got the number right!")
     if user_number !=  magic_numbers:
-        print("You didn't quite get it!")
+        return ("You didn't quite get it!")
+
+magic_numbers = random.randint(0,9)
+
+def run_program_x_times(chances):
+    for attempt in range(chances):
+        print("Attempt number: {}".format(attempt+1))
+        message = ask_user_and_check_reply()
+        print(message)
+
+
+
+run_program_x_times(int(input("How many tries do you want to have?")))
 
 print ("\n\n Coorect answer was: {}".format(magic_numbers))
